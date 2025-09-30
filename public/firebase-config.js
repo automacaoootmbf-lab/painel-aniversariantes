f3cadd81a2062a2f650b0b5cc4a3ed0d6bfe2bee
automacaoootmbf-lab <automacao.ootmbf@gmail.com>
2025-09-30 20:32:51 -0300

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+window.__firebase_config = JSON.stringify({
+  apiKey: "SUA_API_KEY",
+  authDomain: "aniversariantes-automation.firebaseapp.com",
+  projectId: "aniversariantes-automation",
+  storageBucket: "aniversariantes-automation.appspot.com",
+  messagingSenderId: "464841073152",
+  appId: "1:464841073152:web:24401ba9aae664381e2ee5"
+});
