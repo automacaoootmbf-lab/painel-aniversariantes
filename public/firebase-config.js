87ed6adddaa926ddad2cd894cda41188abca8eef
automacaoootmbf-lab <automacao.ootmbf@gmail.com>
2025-10-01 11:03:10 -0300

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,7 +2,7 @@ window.__firebase_config = JSON.stringify({
   apiKey: "AIzaSyBbxc1oFJjdbOnPGyts6SoamCvKGADvZHk",
   authDomain: "aniversariantes-automation.firebaseapp.com",
   projectId: "aniversariantes-automation",
-  storageBucket: "aniversariantes-automation.appspot.com",
+  storageBucket: "aniversariantes-automation.appspot.com", 
   messagingSenderId: "464841073152",
   appId: "1:464841073152:web:24401ba9aae664381e2ee5"
 });
